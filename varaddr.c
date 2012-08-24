025185c6005e25816f8b0a049bd77d46c0a529f7
Dave Thomas <dave.thomas@metaforic.com>
2012-08-24 11:01:36 +0100

    The varaddr page example is likely describing matters in insufficient detail. We must demonstrate that 'N' is re-read from memory. To cause this we can add a barrier, e.g. a function call, or rely on a compiler which has naive variable lifetime analysis (which ADS and GCC don't).

--- varaddr.c
+++ varaddr.c
@@ -1,5 +1,6 @@
 extern int getlimit(int *pN);
 
+/* armcc is clever enough in this case... */
 int sum(const int *array)
 {
     int N;
@@ -15,5 +16,37 @@ int sum(const int *array)
     return sum;
 }
 
-/* armcc is clever enough in this case... */
+/* gcc 4.4.3 resolves the indirected j back into a register (j is an alias for sum) */
+int sum2(const int *array)
+{
+    int N;
+    int sum;
+    int i;
+    int *j = &sum;
+
+    getlimit(&N);
+
+    *j = 0;
+    for (i = 0; i < N; i++)
+        *j += array[i];
+
+    return *j;
+}
+
+/* gcc 4.4.3 seems to show the problem when i add in a function call */
+extern int fn(int);
+int sum3(const int *array)
+{
+    int N;
+    int sum;
+    int i;
+
+    getlimit(&N);
+
+    sum = 0;
+    for (i = 0; i < N; i++)
+        sum += fn(array[i]);
+
+    return sum;
+}
 
